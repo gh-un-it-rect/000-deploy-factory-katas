89a08767bc6e4cb831df2857e4186c5d2b291242
itRect-01 <66125638+itRect-01@users.noreply.github.com>
2020-06-04 00:25:34 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -38,11 +38,8 @@ COMMIT="Reset Repo"
 
 #function __preExecute__  {
 	time=$(date +%s%N)
-	echo $time
-	_f_="__preExecute__"
-	echo $_f_	
 	curl -v -X DELETE -H "Authorization: token $__TOKEN_GITHUB__" https://api.github.com/repos/${__ORG_TARGET_IT_RECT__}/${NEW_REPO_NAME}
-	echo -e "\e[42;1m Runtime ["$_f_"]: $(echo "scale=3;($(date +%s%N) -  ${time})/(1*10^09)" | bc) seconds"	
+	echo -e "\e[42;1m Runtime [PreExecute]: $(echo "scale=3;($(date +%s%N) -  ${time})/(1*10^09)" | bc) seconds"	
 #}
 
 ####################################################
@@ -51,48 +48,28 @@ COMMIT="Reset Repo"
 #
 ####################################################
 #function __execute__ {	
-	echo "__execute__"
 	time=$(date +%s%N)
-	echo $time
-	_f_="__execute__"
-	echo $_f_
 	curl -i -H "$__PREVIEW__" -H "$__JSON__" -H "Authorization: token $__TOKEN_GITHUB__" -d "$__BODY_OK__" https://api.github.com/repos/$__ORG_DEPLOY__/$FOLDER_URL
-	echo 2
 	curl -v -H "Authorization: token ${__TOKEN_GITHUB__}" https://api.github.com/orgs/${__ORG_TARGET_IT_RECT__}/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}' 
-	echo 3
 	git clone ${URL_MASTER} 
-	echo 4
 	rm -Rf  ${NEW_REPO_NAME}
-	echo 5
 	mv ${FOLDER_URL} ${NEW_REPO_NAME}
-	echo 6
 	cd ${NEW_REPO_NAME}
-	echo 7
 	rm -Rf .git 
-	echo 8
 	git init 
-	echo 9
 	git add -A  
-	echo 10
 	git remote add origin https://$__TOKEN_GITHUB__@github.com/${__ORG_TARGET_IT_RECT__}/${NEW_REPO_NAME}.git 
-	echo 11
 	git commit -m "${COMMIT}"
-	echo 12
 	git push --quiet --set-upstream origin master 
-	echo 13
 	git checkout -b develop 
-	echo 14
 	git push --quiet --set-upstream origin develop 
-	echo 15
 	git checkout -b test
-	echo 16
 	git push --quiet --set-upstream origin test 
-	echo 17
-
+	
 	echo -e " \e[42;1m ${MAESTRO} Ejecución satisfactoria ${NEW_REPO_NAME}"
 
 	curl -i -H "$__PREVIEW__" -H "$__JSON__" -H "Authorization: token $__TOKEN_GITHUB__" -d "$__BODY_KO__" https://api.github.com/repos/$__ORG_DEPLOY__/$FOLDER_URL
-	echo -e " \e[42;1m Runtime ["$_f_"]: $(echo "scale=3;($(date +%s%N) -  ${time})/(1*10^09)" | bc) seconds"	    	
+	echo -e " \e[42;1m Runtime [__execute__]: $(echo "scale=3;($(date +%s%N) -  ${time})/(1*10^09)" | bc) seconds"	    	
 #}
 
 #function __main__ {
