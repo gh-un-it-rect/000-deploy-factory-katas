1275ebb4206088b0b8f88cb21171d009641d555c
itRect-01 <66125638+itRect-01@users.noreply.github.com>
2020-06-04 00:11:05 +0200

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,8 +15,6 @@ FOLDER_URL=${BASE_REPO_NAME}-${TYPE_REPO_NAME}
 NEW_REPO_NAME=$1
 URL_MASTER=https://github.com/${__ORG_DEPLOY__}/${FOLDER_URL}.git
 COMMIT="Reset Repo"
-ORG_TARGET=$__ORG_TARGET_IT_RECT__
-
 
 ####################################################
 #
@@ -34,7 +32,7 @@ ORG_TARGET=$__ORG_TARGET_IT_RECT__
 	echo -e "URL_MASTER:\t\t ${URL_MASTER}"
 	echo -e "COMMIT:\t\t\t ${COMMIT}"
 	echo -e "FOLDER_URL:\t\t ${FOLDER_URL}"
- 	echo -e "ORG_TARGET:\t\t ${ORG_TARGET}"
+ 	echo -e "ORG_TARGET:\t\t $__ORG_TARGET_IT_RECT__"
 	echo ""
 #}
 
@@ -43,7 +41,7 @@ ORG_TARGET=$__ORG_TARGET_IT_RECT__
 	echo $time
 	_f_="__preExecute__"
 	echo $_f_	
-	curl -v -X DELETE -H "Authorization: token $__TOKEN_GITHUB__" https://api.github.com/repos/${ORG_TARGET_IT_RECT}/${NEW_REPO_NAME}
+	curl -v -X DELETE -H "Authorization: token $__TOKEN_GITHUB__" https://api.github.com/repos/${__ORG_TARGET_IT_RECT__}/${NEW_REPO_NAME}
 	echo -e " \e[42;1m Runtime ["$_f_"]: $(echo "scale=3;($(date +%s%N) -  ${time})/(1*10^09)" | bc) seconds"	
 #}
 
@@ -60,7 +58,7 @@ ORG_TARGET=$__ORG_TARGET_IT_RECT__
 	echo $_f_
 	curl -i -H "$__PREVIEW__" -H "$__JSON__" -H "Authorization: token $__TOKEN_GITHUB__" -d "$__BODY_OK__" https://api.github.com/repos/$__ORG_DEPLOY__/$FOLDER_URL
 	echo 2
-	curl -v -H "Authorization: token ${__TOKEN_GITHUB__}" https://api.github.com/orgs/${ORG_TARGET_IT_RECT}/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}' 
+	curl -v -H "Authorization: token ${__TOKEN_GITHUB__}" https://api.github.com/orgs/${__ORG_TARGET_IT_RECT__}/repos -d '{"name": "'"${NEW_REPO_NAME}"'"}' 
 	echo 3
 	git clone ${URL_MASTER} 
 	echo 4
@@ -76,7 +74,7 @@ ORG_TARGET=$__ORG_TARGET_IT_RECT__
 	echo 9
 	git add -A  
 	echo 10
-	git remote add origin https://$__TOKEN_GITHUB__@github.com/${ORG_TARGET_IT_RECT}/${NEW_REPO_NAME}.git 
+	git remote add origin https://$__TOKEN_GITHUB__@github.com/${__ORG_TARGET_IT_RECT__}/${NEW_REPO_NAME}.git 
 	echo 11
 	git commit -m "${COMMIT}"
 	echo 12
