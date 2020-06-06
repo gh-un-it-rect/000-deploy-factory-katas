451a9a8bcc824a2378e57de4136e82aa5eebd130
Victor Bolinches <vicboma1@users.noreply.github.com>
2020-06-06 15:47:50 +0200

    Update cipher-decrypt.sh

--- scripts/cipher-decrypt.sh
+++ scripts/cipher-decrypt.sh
@@ -1,1 +1,1 @@
-echo $1 | openssl enc -aes-256-cbc -md sha512 -iter 100000 -a -d -salt -pass pass:$__CIPHER_KEY__
+echo $1 | openssl enc -aes-256-cbc -md sha512 -a -d -salt -pass pass:$__CIPHER_KEY__
